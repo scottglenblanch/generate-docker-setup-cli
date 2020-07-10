8e72710772c6f2fdf6fd369f42ef927dd03a331d
Scott Blanch <scottglenblanch@gmail.com>
2020-07-10 13:25:52 -0700

    Fri Jul 10 13:25:52 PDT 2020

--- templates/docker/scripts/host/run-container.sh
+++ templates/docker/scripts/host/run-container.sh
@@ -8,8 +8,7 @@ run_container() {
 
 set_variables() {
   SCRIPT_RUNNING_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-  # will set TAG if has argument --tag <some value>
-  source /dev/stdin <<< "$(curl https://raw.githubusercontent.com/scottglenblanch/bash-argument-parser/main/src/arg-parser.sh)"
+  TAG="$("${SHARED_SCRIPTS_DIR}/get-tag-name.sh")"
 }
 
 set_variables

3def383127a543aa1656c75bdc9d8c8c4e9d6ac2
Scott Blanch <scottglenblanch@gmail.com>
2020-07-10 13:33:41 -0700

    Fri Jul 10 13:33:41 PDT 2020

--- templates/docker/scripts/host/run-container.sh
+++ templates/docker/scripts/host/run-container.sh
@@ -12,8 +12,6 @@ set_variables() {
   SCRIPT_RUNNING_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
   SHARED_SCRIPTS_DIR="${SCRIPT_RUNNING_DIR}/shared"
   TAG="$("${SHARED_SCRIPTS_DIR}/get-tag-name.sh")"
-
-  echo "TAG ${TAG}"
 }
 
 set_variables

8404e4f4dd7590bf735f89cb773e28e15c1903f0
Scott Blanch <scottglenblanch@gmail.com>
2020-08-01 15:24:06 -0600

    Sat Aug  1 15:24:06 MDT 2020

--- templates/docker/scripts/custom-scripts/start-app.sh
+++ templates/docker/scripts/custom-scripts/start-app.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-echo 'change code in ./docker/scripts/custom-scripts/start-app.sh to get app to start'
+echo 'change code on host machine in
+<host root>/docker/scripts/custom-scripts/start-app.sh
+to get app to start'
